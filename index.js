4a589072c976718d4604ecb5d642eaa705d3be58
bkrkic <139828582+bkrkic@users.noreply.github.com>
2023-07-27 09:36:46 +1000

    Partner changes.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-console.log("Hello World", sum_of_ab)
-
-//Task 4
-function sum_of_ab(a, b) {
-    return a+b;
-}
-
-let a = 10;
-let b = 5;
-console.log(sum_of_ab(a, b));
+console.log("I caused a merge conflict.")
